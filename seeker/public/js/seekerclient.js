8ce2f7a7c1718e831916213458981a80cbca5a7e
Evan Griffiths <griffithse@gmail.com>
2015-06-04 12:57:41 -0400

    login works

--- seeker/public/js/seekerclient.js
+++ seeker/public/js/seekerclient.js
@@ -1,4 +1,89 @@
-angular.module('seeker',[])
-  .controller('main', function($scope) {
-    $scope.msg = 'hi';
+angular.module('seeker',['ngRoute'])
+  .controller('nav', function($location, $rootScope, $scope, $window) {
+    $rootScope.$on("$locationChangeStart", function(event,next,current) {
+      if (typeof $window.sessionStorage.token === "undefined" && next.indexOf("#/app/") > -1) {
+        event.preventDefault();
+      }
+    });
+    $scope.logout = function() {
+      delete $window.sessionStorage.token;
+    };
+  })
+
+  .controller('home', function($scope, $http) {
+    // $scope.message = "welcome home";
+    $http.get('/api/test')
+      .success(function(data) {
+        $scope.message = data;
+      });
+  })
+
+  .controller('login', function($scope,$http,$window) {
+    $scope.submit = function() {
+      $http.post("/login", {user: $scope.user})
+        .success(function(data) {
+          $window.sessionStorage.token = data.token;
+          alert('yay');
+        })
+        .error(function(err) {
+          delete $window.sessionStorage.token;
+          alert(err);
+        });
+    }
+  })
+  .controller('signup', function($scope,$http) {
+    $scope.user = {};
+    $scope.submit = function() {
+      if ($scope.user.password !== $scope.user.confirmpassword) {
+        alert("passwords don't match");
+      } else {
+        $http.post("/signup",{user: $scope.user})
+          .success(function(res,status) {
+            alert(res + " " + status);
+          })
+          .error(function(res,status) {
+            alert("err: "+res+" status: "+status);
+          });
+      }
+    }
+  })
+
+  .factory('authInterceptor', function($q, $window, $location) {
+    return {
+      request: function(config) {
+        config.headers = config.headers || {};
+        if ($window.sessionStorage.token) {
+          config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+        }
+        return config;
+      },
+      response: function(response) {
+        if (response.status === 401) {
+          delete $window.sessionStorage.token;
+          $location.path('login');
+        }
+        return response || $q.when(response);
+      }
+    };
+  })
+
+  .config(function($routeProvider, $httpProvider) {
+    $httpProvider.interceptors.push('authInterceptor');
+
+    $routeProvider
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'login'
+      })
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'signup'
+      })
+      .when('/app/home', {
+        templateUrl: 'views/home.html',
+        controller: 'home'
+      })
+      .otherwise({
+        redirectTo: 'login'
+      });
   });
--- seeker/seeker.js
+++ seeker/seeker.js
@@ -1,14 +1,107 @@
+var secrets = require('../secrets.json');
+var assert = require('assert');
+var Q = require('q');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var forge = require('node-forge');
+
 var app = express();
-// app.use(bodyParser.urlencoded({extended: false}));
+// any route prefixed with api will be authenticated
+app.use('/api',expressJwt({secret:secrets.jwt}));
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
+// deal with jwt unauthorized sessions without spamming the terminal
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).send('Not logged in');
+  }
+});
+
+var Mongo = require('mongodb');
+var MongoClient = Mongo.MongoClient;
+var ObjectId = Mongo.ObjectID;
+var dburl = 'mongodb://localhost:27017/jobsorter';
+
+// MongoClient.connect(dburl, function(err,db) {
+//   assert.equal(null, err);
+//   console.log("Connected to mongo server.");
+//   db.close();
+// });
 
 app.get('/', function(req,res) {
-  res.redirect('index.html');
+  res.send('hi?');
+  // res.redirect('/login');
+});
+
+app.get('/api/test', function(req,res) {
+  res.send('secret data lies within!');
+});
+
+// var dbconnect = Q.nbind(MongoClient.connect, MongoClient, dburl); 
+
+app.post('/login', function(req,res) {
+  MongoClient.connect(dburl, function(err,db) {
+    var cursor = db.collection('users').find({email: req.body.user.email}).limit(1);
+    cursor.next(function(err,doc) {
+      db.close();
+      assert.equal(err, null);
+      if (doc != null) {
+        // hash entered password using stored salt and see if it matches the stored password_digest
+        var password_digest = doc.password.slice(0,-16);
+        var salt = doc.password.slice(-16);
+        var md = forge.md.sha256.create();
+        md.update(req.body.user.password + salt);
+        if (md.digest().toHex() === password_digest) {
+          //password matches so log in by passing token to client
+          var profile = {
+            email: doc.email,
+            id: doc._id
+          }
+          var token = jwt.sign(profile,secrets.jwt, {expiresInMinutes:60*5});
+          res.json({token:token});
+        } else {
+          //doesn't match so return error
+          res.status(404).send('wrong password');
+        }
+      } else {
+        res.status(404).send('user not found');
+      }
+    });
+  });
+});
+
+app.post('/signup', function(req,res) {
+  MongoClient.connect(dburl, function(err,db) {
+    db.collection('users').find({email: req.body.user.email}).limit(1).count(function(err,count) {
+      if (count > 0) {
+        // email exists already so send error to client
+        db.close();
+        res.status(403).send("user already exists");
+      } else {
+        // email address not in db, so we can add our user
+        var md = forge.md.sha256.create();
+        var salt = forge.random.getBytesSync(16);
+        md.update(req.body.user.password + salt);
+        var password_digest = md.digest().toHex() + salt;
+
+        db.collection('users').insertOne({
+          email: req.body.user.email,
+          password: password_digest
+        }, function(err,result) {
+          assert.equal(null,err);
+          db.close();
+          res.send(result);
+        });
+      }
+    });
+  });
 });
 
 app.listen(3000, function(){console.log('seekerclient started on port 3000');});
