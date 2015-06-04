662a6c66903477a786c191f5187b9f87112c73c4
Steve McIntosh <scm.phub@gmail.com>
2015-06-04 15:39:47 -0400

    steve's changes

--- seeker/public/js/seekerclient.js
+++ seeker/public/js/seekerclient.js
@@ -1,4 +1,40 @@
-angular.module('seeker',[])
-  .controller('main', function($scope) {
+angular.module('seeker',['ngRoute'])
+  .controller('main', function($scope, $location) {
     $scope.msg = 'hi';
+    
+    $scope.doSearch = function() {
+      $location.path('/search').search({'name' : 'dave'});
+      // alert("In main controller.");
+    }
+
+    $scope.seeview2 = function() {
+      $location.path('/view2');  
+    }
+
+  })
+
+  .controller('search', function($scope, $location, $http) {
+    $scope.msg = JSON.stringify($location.search());
+    $http.get('http://localhost:3000/search')
+      .success(function(data) {
+        $scope.data = data;
+      })
+      .error(function(err) {
+        alert("Error:" + err);
+      });
+    $scope.detailView = function(row) {
+      alert(row);
+    }
+  })
+
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/search', {
+        templateUrl: 'views/search.html',
+        controller: 'search'
+      })
+
+      .when('/view2', {
+        templateUrl: 'views/view2.html'
+      });
   });
--- seeker/seeker.js
+++ seeker/seeker.js
@@ -11,4 +11,8 @@ app.get('/', function(req,res) {
   res.redirect('index.html');
 });
 
+app.get('/search', function(req,res) {
+  res.json([1,2,3,4,5]);
+});
+
 app.listen(3000, function(){console.log('seekerclient started on port 3000');});
