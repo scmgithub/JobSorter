1bcfd6bed8f2821bfd4d32275d7543575fa34737
sleepylemur <griffithse@gmail.com>
2015-06-07 14:21:32 -0400

    messing about in python

--- gensimtest/makedict.py
+++ gensimtest/makedict.py
@@ -2,7 +2,6 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 from gensim import corpora, models, similarities, utils
-
 import pymongo
 
 from pymongo import MongoClient
@@ -10,5 +9,4 @@ client = MongoClient()
 db = client.jobsorter
 
 dict = corpora.Dictionary( utils.simple_preprocess(job['job_detail']) for job in db.joblistings.find({},{'job_detail':1}) )
-
 dict.save('tmp/dict')
